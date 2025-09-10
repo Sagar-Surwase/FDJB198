db39619c7a406b53a65ff33885aac56b7d924008
Sagar-Surwase <surwasesagar8888@gmail.com>
2025-09-10 14:25:09 +0530

    Update Test.java

--- Demo_Project/src/Calculator/Test.java
+++ Demo_Project/src/Calculator/Test.java
@@ -5,9 +5,13 @@ public class Test
 	
 	public void m1()
 	{
-		System.out.println("dcdsj");
+		System.out.println("This method is created for Pull......");
 		System.out.println("dcdsj");
 		System.out.println("Git Token Genration.....");
 	}
 
+	public void m2()
+	{
+		System.out.println("This method is created for Pull......");
+	}
 }
