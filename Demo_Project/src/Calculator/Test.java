f1475541cda6cc727db3434a7ec3a2803ebfbbd0
Sagar-Surwase <surwasesagar8888@gmail.com>
2025-09-10 15:01:34 +0530

    commit

--- Demo_Project/src/Calculator/Test.java
+++ Demo_Project/src/Calculator/Test.java
@@ -5,7 +5,7 @@ public class Test
 	
 	public void m1()
 	{
-		System.out.println("This method is created for Pull......");
+		System.out.println("This method is created for Push......");
 		System.out.println("dcdsj");
 		System.out.println("Git Token Genration.....");
 	}
@@ -13,5 +13,10 @@ public class Test
 	public void m2()
 	{
 		System.out.println("This method is created for Pull......");
+	}
+	
+	public void m3()
+	{
+		System.out.println("This method is created for Push-1.............");
 	}
 }
